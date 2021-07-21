71c510a7f18c5b3ede8b83b72acfb1e9a4a9de4a
dieuhuyphuc <dieuhuyphuc@gmail.com>
2021-07-21 16:38:18 +0700

    update

--- app/Http/Controllers/ApartmentsController.php
+++ app/Http/Controllers/ApartmentsController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ApartmentsController extends Controller
 {
-    public function index(){
-        return view ('apartments.Aparstments');
+    public function create(){
+        return view ('apartments');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/apartments', [\App\Http\Controllers\ApartmentsController::class, 'create']);
